bbe8ecdc0897e8028bc6ee1731d88ce88b5976bb
GunaGGowda <150352153+GunaGGowda@users.noreply.github.com>
2025-12-08 07:14:15 +0530

    Update script.js

--- Assign2/script.js
+++ Assign2/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    $("#registrationForm").on("submit", function () {
+    $("#regForm").submit(function (event) {
         alert("Your registration form is being submitted...");
-        return true; // allow form submission
     });
 });
